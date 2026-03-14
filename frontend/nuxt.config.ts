7d929d8358cff9c2334065d33ed4889554a3df46
Rekar Jamal <rekarjamal33@gmail.com>
2026-03-14 19:17:11 +0300

    fixing folder structure

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -5,12 +5,29 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+
   devServer: {
     port: 3020,
   },
+
   vite: {
     plugins: [
       tailwindcss()
     ]
-  }
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: '@/components/ui'
+  },
+  modules: ['@pinia/nuxt', '@nuxt/image', 'shadcn-nuxt']
 })
